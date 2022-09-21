e98b6f98316f1f1324257cca3c87f7a7a8a470f3
abdul <malik301002@gmail.com>
2022-09-21 13:18:50 +0700

    feat: customize font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,13 +3,16 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      poppins: 'Poppins, sans-serif'
+    },
     colors: {
       primary: '#16ABF8',
       secondary: '#E5E5E5',
       danger: '#ED4C5C',
       white: '#FFFFFF',
       black: '#111111',
-      gray: '#555555'
+      light: '#F4F4F4'
     }
   },
   plugins: []
