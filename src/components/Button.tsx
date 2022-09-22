cdc188b2930c489aad571f02ed0cef8a2026a7f1
abdul <malik301002@gmail.com>
2022-09-22 21:55:42 +0700

    feat(comps): 🎸 add hover effect to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,10 +10,10 @@ export const Button = (props: ButtonProps) => {
   const { children, color } = props;
 
   const classes: string = clsx(
-    'w-[150px] h-[54px] rounded-full font-bold',
-    color === 'primary' && 'bg-primary text-white',
-    color === 'danger' && 'bg-danger text-white',
-    color === 'light' && 'bg-light text-black'
+    'w-[150px] h-[54px] rounded-full text-lg font-bold',
+    color === 'primary' && 'bg-primary text-white hover:bg-primary/[.85]',
+    color === 'danger' && 'bg-danger text-white hover:bg-danger/[.85]',
+    color === 'light' && 'bg-light text-black hover:bg-black/[.2]'
   );
 
   return <button className={classes}>{children}</button>;
