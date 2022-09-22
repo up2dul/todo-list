052cc99d0e58805c8122e3ed18faa4f1bc9abc27
abdul <malik301002@gmail.com>
2022-09-22 20:46:11 +0700

    feat(pages): 🎸 add pages

---
+++ src/pages/Activity.tsx
@@ -0,0 +1,10 @@
+import { BaseLayout } from '@/layouts';
+
+export const Activity = () => {
+  return (
+    <BaseLayout>
+      <h1>Todo list page</h1>
+      <h1>asiap</h1>
+    </BaseLayout>
+  );
+};
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,9 @@
+import { BaseLayout } from '@/layouts';
+
+export const Dashboard = () => {
+  return (
+    <BaseLayout>
+      <h1>Activity page</h1>
+    </BaseLayout>
+  );
+};
