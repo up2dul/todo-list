6fb60112600116c87fb87f2d2f2ee2d9230f9f75
abdul <malik301002@gmail.com>
2022-09-22 21:58:19 +0700

    feat(pages): 🎸 Dashboard / home pages content

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,18 @@
+import { Button } from '@/components';
 import { BaseLayout } from '@/layouts';
+
+import addActivitySvg from '@/assets/svg/add-activity.svg';
 
 export const Dashboard = () => {
   return (
     <BaseLayout>
-      <h1>Activity page</h1>
+      <div className='flex items-center justify-between'>
+        <h1 className='text-4xl font-bold'>Activity</h1>
+        <Button color='primary'>+ Tambah</Button>
+      </div>
+      <div className='flex justify-center'>
+        <img src={addActivitySvg} alt='add first activity' className='mt-16 cursor-pointer' />
+      </div>
     </BaseLayout>
   );
 };
