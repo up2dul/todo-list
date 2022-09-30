2b2e320fdc6505fd0fd407b28635a8ef035fcb35
abdul <malik301002@gmail.com>
2022-09-30 21:11:47 +0700

    refactor(comps): 💡 change children type from string to ReactNode

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,15 @@
+import { ReactNode } from 'react';
 import clsx from 'clsx';
 
 type ButtonProps = {
-  children: string;
+  children: ReactNode;
   color: 'primary' | 'danger' | 'light';
   isDisabled?: boolean;
 };
 
-export const Button = (props: ButtonProps) => {
-  const { children, color } = props;
-
+export const Button = ({ children, color }: ButtonProps) => {
   const classes: string = clsx(
-    'w-[150px] h-[54px] rounded-full text-lg font-bold',
+    'w-[150px] h-[54px] rounded-full text-lg font-medium',
     color === 'primary' && 'bg-primary text-white hover:bg-primary/[.85]',
     color === 'danger' && 'bg-danger text-white hover:bg-danger/[.85]',
     color === 'light' && 'bg-light text-black hover:bg-black/[.2]'
