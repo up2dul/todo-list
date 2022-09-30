528f756044c3eb0f65713788464cbe470cd5b22b
abdul <malik301002@gmail.com>
2022-10-01 01:11:01 +0700

    feat(pages): 🎸 add dummy content to dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,17 +1,32 @@
-import { Button } from '@/components';
-import { BaseLayout } from '@/layouts';
+import { TbPlus } from 'react-icons/tb';
 
-import addActivitySvg from '@/assets/svg/add-activity.svg';
+import { BaseLayout } from '@/layouts';
+import { ActivityCard, Button } from '@/components';
+
+// import addActivitySvg from '@/assets/svg/add-activity.svg';
 
 export const Dashboard = () => {
   return (
     <BaseLayout>
       <div className='flex items-center justify-between'>
-        <h1 className='text-4xl font-bold'>Activity</h1>
-        <Button color='primary'>+ Tambah</Button>
+        <h1 data-cy='activity-title' className='text-4xl font-bold text-black'>
+          Activity
+        </h1>
+
+        <Button cy='activity-add-button' color='primary'>
+          <TbPlus /> Tambah
+        </Button>
       </div>
-      <div className='flex justify-center'>
-        <img src={addActivitySvg} alt='add first activity' className='mt-16 cursor-pointer' />
+
+      {/* <div className='mt-24 flex justify-center'>
+        <img data-cy='activity-empty-state' src={addActivitySvg} alt='add first activity' className='cursor-pointer' />
+      </div> */}
+
+      <div className='mt-12 grid gap-x-5 gap-y-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+        <ActivityCard cy='activity-item-0' title='Meeting dengan client' date='21 Oktober 2021' />
+        <ActivityCard cy='activity-item-1' title='Bertemu dokter' date='22 Oktober 2021' />
+        <ActivityCard cy='activity-item-2' title='Mengerjakan skripsi' date='5 Oktober 2021' />
+        <ActivityCard cy='activity-item-3' title='Daftar belanja bulanan' date='3 Oktober 2021' />
       </div>
     </BaseLayout>
   );
