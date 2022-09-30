9418266d4ac552176859a822889cc864a839d7f0
abdul <malik301002@gmail.com>
2022-10-01 01:11:32 +0700

    feat(comps): 🎸 add dataset cy to Button props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,17 +3,22 @@ import clsx from 'clsx';
 
 type ButtonProps = {
   children: ReactNode;
+  cy: string;
   color: 'primary' | 'danger' | 'light';
   isDisabled?: boolean;
 };
 
-export const Button = ({ children, color }: ButtonProps) => {
+export const Button = ({ children, cy, color }: ButtonProps) => {
   const classes: string = clsx(
-    'w-[150px] h-[54px] rounded-full text-lg font-medium',
+    'flex h-[54px] w-[150px] items-center justify-center rounded-full text-lg font-medium',
     color === 'primary' && 'bg-primary text-white hover:bg-primary/[.85]',
     color === 'danger' && 'bg-danger text-white hover:bg-danger/[.85]',
     color === 'light' && 'bg-light text-black hover:bg-black/[.2]'
   );
 
-  return <button className={classes}>{children}</button>;
+  return (
+    <button data-cy={cy} className={classes}>
+      {children}
+    </button>
+  );
 };
