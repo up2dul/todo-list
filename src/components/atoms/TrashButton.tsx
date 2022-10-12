8c9db8c3618e8390ede62f688fc04637833f5d3c
abdul <malik301002@gmail.com>
2022-10-12 21:26:27 +0700

    feat(comps): 🎸 add onClick props to TrashButton

--- src/components/atoms/TrashButton.tsx
+++ src/components/atoms/TrashButton.tsx
@@ -1,7 +1,15 @@
 import { TbTrash } from 'react-icons/tb';
 
-export const TrashButton = ({ buttonType }: { buttonType: 'activity' | 'todo' }) => (
-  <span data-cy={`${buttonType}-item-delete-button`} title={`Delete ${buttonType}`}>
+type TrashButtonProps = {
+  buttonType: 'activity' | 'todo';
+  onClick: () => void;
+};
+
+export const TrashButton = ({ buttonType, onClick }: TrashButtonProps) => (
+  <span
+    data-cy={`${buttonType}-item-delete-button`}
+    title={`Delete ${buttonType}`}
+    onClick={() => onClick()}>
     <TbTrash className='cursor-pointer text-2xl text-dark-3 hover:text-dark-1' />
   </span>
 );
