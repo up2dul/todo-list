83fde4aff1d63c9682d469f143e57363e1687b7e
abdul <malik301002@gmail.com>
2022-10-13 09:31:44 +0700

    feat(pages): 🎸 fetching data to pages

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,21 @@
 import { TbPlus } from 'react-icons/tb';
 
+import { create } from '@/services/api/activity';
+import { useActivity } from '@/services/store';
 import { BaseLayout } from '@/components/layouts';
-import { ActivityCard, Button } from '@/components';
+import { ActivityList, Button } from '@/components';
 
-// import addActivitySvg from '@/assets/svg/add-activity.svg';
+import addActivitySvg from '@/assets/svg/add-activity.svg';
 
 export const Dashboard = () => {
+  const { activities, addActivityState } = useActivity((state) => state);
+
+  const addActivity = async () => {
+    await create()
+      .then((res) => addActivityState(res.data))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <BaseLayout>
       <div className='flex items-center justify-between'>
@@ -13,22 +23,23 @@ export const Dashboard = () => {
           Activity
         </h1>
 
-        <Button cy='activity-add-button' onClick={() => console.log('ntar dlu')} color='primary'>
+        <Button cy='activity-add-button' onClick={addActivity} color='primary'>
           <TbPlus /> Tambah
         </Button>
       </div>
 
-      {/* illustration */}
-      {/* <div className='mt-24 flex justify-center'>
-        <img data-cy='activity-empty-state' src={addActivitySvg} alt='add first activity' className='cursor-pointer' />
-      </div> */}
+      {activities.length < 1 && (
+        <div className='mt-24 flex justify-center'>
+          <img
+            data-cy='activity-empty-state'
+            src={addActivitySvg}
+            alt='add first activity'
+            className='cursor-pointer'
+          />
+        </div>
+      )}
 
-      <div className='mt-12 grid gap-x-5 gap-y-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-        <ActivityCard cy='activity-item-0' title='Meeting dengan client' date='21 Oktober 2021' />
-        <ActivityCard cy='activity-item-1' title='Bertemu dokter' date='22 Oktober 2021' />
-        <ActivityCard cy='activity-item-2' title='Mengerjakan skripsi' date='5 Oktober 2021' />
-        <ActivityCard cy='activity-item-3' title='Daftar belanja bulanan' date='3 Oktober 2021' />
-      </div>
+      <ActivityList />
     </BaseLayout>
   );
 };
--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,22 +1,36 @@
-import { useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { TbCheck, TbChevronLeft, TbPencil, TbPlus } from 'react-icons/tb';
+import { useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { TbChevronLeft, TbPencil, TbPlus } from 'react-icons/tb';
 
-import { useClickOutside } from '@/hooks';
+import { update } from '@/services/api/activity';
+import { useActivity } from '@/services/store';
 import { BaseLayout, Overlay } from '@/components/layouts';
 import { Button, ModalAdd, SortButton, TodoItem } from '@/components';
 
 // import addTodoList from '@/assets/svg/add-todo-list.svg';
 
 export const Detail = () => {
-  const [todoTitle, setTodoTitle] = useState<string>('New activity');
-  const [isEditTodoTitle, setIsEditTodoTitle] = useState<boolean>(false);
+  const { activityId } = useParams();
+
+  const { updateActivityState } = useActivity((state) => state);
+  const activity = useActivity(
+    (state) => state.activities.filter((a) => a.id + '' === activityId)[0]
+  );
+
+  const [title, setTitle] = useState<string>(activity?.title);
   const [isModal, setIsModal] = useState<boolean>(false);
-  const inputRef = useRef<HTMLInputElement>(null);
 
-  const handleClickOutside = () => setIsEditTodoTitle(false);
-
-  useClickOutside(inputRef, handleClickOutside);
+  const handleBlur = async () => {
+    const newData = activity;
+    newData.title = title;
+    try {
+      await update(activityId, newData);
+      updateActivityState(newData);
+    } catch (err) {
+      console.log(err);
+    }
+    console.log(title);
+  };
 
   return (
     <>
@@ -32,28 +46,17 @@ export const Detail = () => {
               </span>
             </Link>
 
-            {!isEditTodoTitle ? (
-              <div onClick={() => setIsEditTodoTitle(true)}>
-                <h1 data-cy='todo-title' className='text-4xl font-bold text-dark-1'>
-                  {todoTitle}
-                </h1>
-              </div>
-            ) : (
-              <input
-                ref={inputRef}
-                type='text'
-                value={todoTitle}
-                maxLength={26}
-                onChange={({ target }) => setTodoTitle(target.value)}
-                className='w-3/4 border-b bg-light-2 text-4xl font-bold text-dark-1 outline-none'
-                autoFocus
-              />
-            )}
-            <span
-              data-cy='todo-title-edit-button'
-              onClick={() => setIsEditTodoTitle(true)}
-              className='cursor-pointer text-2xl text-dark-3'>
-              {!isEditTodoTitle ? <TbPencil /> : <TbCheck />}
+            <input
+              type='text'
+              value={title}
+              maxLength={26}
+              onChange={(e) => setTitle(e.target.value)}
+              onBlur={handleBlur}
+              className='w-3/4 bg-light-2 text-4xl font-bold text-dark-1 outline-none focus:border-b'
+            />
+
+            <span data-cy='todo-title-edit-button' className='cursor-pointer text-2xl text-dark-3'>
+              <TbPencil />
             </span>
           </div>
 
@@ -65,20 +68,19 @@ export const Detail = () => {
           </div>
         </div>
 
-        {/* illustration */}
-        {/* <div className='mt-24 flex justify-center'>
+        <div className='mt-12'>
+          <TodoItem />
+        </div>
+      </BaseLayout>
+      {/* illustration */}
+      {/* <div className='mt-24 flex justify-center'>
         <img
           data-cy='todo-empty-state'
           src={addTodoList}
           alt='add first todo'
           className='cursor-pointer'
         />
-      </div> */}
-        <div className='mt-12'>
-          <TodoItem />
-        </div>
-      </BaseLayout>
-
+        </div> */}
       {isModal && (
         <Overlay>
           <ModalAdd handleClickOutside={() => setIsModal(false)} />
