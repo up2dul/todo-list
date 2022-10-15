a8509cc41c668be3f511096a4a41841016bd20c9
abdul <malik301002@gmail.com>
2022-10-15 13:13:29 +0700

    feat(comps): 🎸 add TodoItem props

--- src/components/molecules/TodoItem.tsx
+++ src/components/molecules/TodoItem.tsx
@@ -1,18 +1,22 @@
 import { TbPencil } from 'react-icons/tb';
 
-import { TrashButton } from '@/components';
+import type { Priority } from '@/types';
+import { PriorityIndicator, TrashButton } from '@/components';
 
-export const TodoItem = () => (
+type TodoItemProps = {
+  title: string;
+  priority: Priority;
+};
+
+export const TodoItem = ({ title, priority }: TodoItemProps) => (
   <div className='flex items-center justify-between rounded-xl bg-light-1 p-7 shadow-md'>
     <div className='flex items-center gap-4'>
       <input data-cy='todo-item-checkbox' type='checkbox' className='h-5 w-5' />
 
-      <div
-        data-cy='todo-item-priority-indicator'
-        className='h-2.5 w-2.5 rounded-full bg-danger'></div>
+      <PriorityIndicator priority={priority} />
 
       <h1 data-cy='todo-item-title' className='text-lg font-medium text-dark-1'>
-        Telur ayam
+        {title}
       </h1>
 
       <span data-cy='todo-item-edit-button' className='cursor-pointer text-xl text-dark-3'>
