f93f13a395ab639f8376af59b8422c14bac5ffcc
abdul <malik301002@gmail.com>
2022-10-15 20:25:05 +0700

    feat(types): 🎸 create some types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,26 @@
 import { ReactNode } from 'react';
 
-export type DropdownItemProps = {
+export type TodoSortProps = {
   cy: string;
   icon: ReactNode;
   title: string;
   isChecked: boolean;
 };
 
+export type Todo = {
+  id: number;
+  title: string;
+  activity_group_id: number;
+  is_active: boolean;
+  priority: Priority;
+};
+
 export type Priority = 'very-high' | 'high' | 'medium' | 'low' | 'very-low';
+
+export type SortType =
+  | 'sort-latest'
+  | 'sort-oldest'
+  | 'sort-az'
+  | 'sort-za'
+  | 'sort-unfinished'
+  | string;
