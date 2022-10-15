f5d19a682bbcea62e3b974c228770ce4c44177ce
abdul <malik301002@gmail.com>
2022-10-16 00:21:19 +0700

    feat(comps): 🎸 add some props to TodoItem component

--- src/components/molecules/TodoItem.tsx
+++ src/components/molecules/TodoItem.tsx
@@ -1,21 +1,41 @@
+/* eslint-disable no-unused-vars */
+import { ChangeEvent } from 'react';
+import clsx from 'clsx';
 import { TbPencil } from 'react-icons/tb';
 
 import type { Priority } from '@/types';
 import { PriorityIndicator, TrashButton } from '@/components';
 
 type TodoItemProps = {
+  cy: number;
   title: string;
   priority: Priority;
+  isActive: boolean;
+  onCheck: (e: ChangeEvent<HTMLInputElement>) => void;
+  onDelete: () => void;
 };
 
-export const TodoItem = ({ title, priority }: TodoItemProps) => (
-  <div className='flex items-center justify-between rounded-xl bg-light-1 p-7 shadow-md'>
+export const TodoItem = ({ cy, title, priority, isActive, onCheck, onDelete }: TodoItemProps) => (
+  <div
+    data-cy={'todo-item-' + cy}
+    className='flex items-center justify-between rounded-xl bg-light-1 p-7 shadow-md'>
     <div className='flex items-center gap-4'>
-      <input data-cy='todo-item-checkbox' type='checkbox' className='h-5 w-5' />
+      <input
+        data-cy='todo-item-checkbox'
+        type='checkbox'
+        className='h-5 w-5'
+        checked={!isActive}
+        onChange={onCheck}
+      />
 
       <PriorityIndicator priority={priority} />
 
-      <h1 data-cy='todo-item-title' className='text-lg font-medium text-dark-1'>
+      <h1
+        data-cy='todo-item-title'
+        className={clsx(
+          'text-lg font-medium text-dark-1',
+          !isActive && 'text-dark-2 line-through'
+        )}>
         {title}
       </h1>
 
@@ -24,6 +44,6 @@ export const TodoItem = ({ title, priority }: TodoItemProps) => (
       </span>
     </div>
 
-    <TrashButton buttonType='todo' onClick={() => console.log('temporary handler')} />
+    <TrashButton buttonType='todo' onClick={onDelete} />
   </div>
 );
