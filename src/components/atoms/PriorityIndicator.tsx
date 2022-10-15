1fb9fe47b7dd739d0a889f6e09c9aed7ef532769
abdul <malik301002@gmail.com>
2022-10-15 13:05:20 +0700

    feat(comps): 🎸 create PriorityIndicator component

---
+++ src/components/atoms/PriorityIndicator.tsx
@@ -0,0 +1,33 @@
+import type { Priority } from '@/types';
+
+type PriorityIndicatorProps = {
+  priority: Priority;
+};
+
+export const PriorityIndicator = ({ priority }: PriorityIndicatorProps) => {
+  let bgColor;
+
+  switch (priority) {
+    case 'very-high':
+      bgColor = 'bg-p-very-high';
+      break;
+    case 'high':
+      bgColor = 'bg-p-high';
+      break;
+    case 'medium':
+      bgColor = 'bg-p-medium';
+      break;
+    case 'low':
+      bgColor = 'bg-p-low';
+      break;
+    case 'very-low':
+      bgColor = 'bg-p-very-low';
+      break;
+  }
+
+  return (
+    <div
+      data-cy='todo-item-priority-indicator'
+      className={`h-2.5 w-2.5 rounded-full ${bgColor}`}></div>
+  );
+};
--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -2,5 +2,6 @@ export * from './Button';
 export * from './Header';
 export * from './SortItem';
 export * from './PriorityItem';
+export * from './PriorityIndicator';
 export * from './TrashButton';
 export * from './AlertDelete';
