481b6d3d3080893d8247aa608480b32338835453
abdul <malik301002@gmail.com>
2022-10-15 13:04:49 +0700

    refactor(.): 💡 rename priority custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,11 +15,11 @@ module.exports = {
       'dark-3': '#888888',
       'light-1': '#FFFFFF',
       'light-2': '#F4F4F4',
-      'priority-vh': '#ED4C5C',
-      'priority-h': '#F8A541',
-      'priority-m': '#00A790',
-      'priority-l': '#428BC1',
-      'priority-vl': '#8942C1'
+      'p-very-high': '#ED4C5C',
+      'p-high': '#F8A541',
+      'p-medium': '#00A790',
+      'p-low': '#428BC1',
+      'p-very-low': '#8942C1'
     }
   },
   plugins: []
