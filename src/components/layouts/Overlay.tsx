8db998684d38f128f075fc02d3f7bf7cbbe8f9db
abdul <malik301002@gmail.com>
2022-10-16 00:02:51 +0700

    fix(comps): 🐛 inset bug

--- src/components/layouts/Overlay.tsx
+++ src/components/layouts/Overlay.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 
 export const Overlay = ({ children }: { children: ReactNode }) => (
-  <div className='fixed top-0 flex h-full w-full items-center justify-center bg-dark-3/50'>
+  <div className='fixed left-0 top-0 flex h-full w-full items-center justify-center bg-dark-3/50'>
     {children}
   </div>
 );
