60de87adb38e7053f2e6527814b653e043649698
abdul <malik301002@gmail.com>
2022-10-18 12:57:49 +0700

    feat(store): 🎸 add some properties and method to useModalTodo store

--- src/services/store/use-modal-todo.ts
+++ src/services/store/use-modal-todo.ts
@@ -7,12 +7,16 @@ type Modal = {
   id: number;
   title: string;
   priority: PriorityType;
+  activity_group_id?: number;
 };
 
 type ModalTodo = {
   modal: Modal;
+  type: 'add' | 'edit';
   isShow: boolean;
   setModal: (newData: Modal) => void;
+  resetModal: () => void;
+  setModalType: (type: 'add' | 'edit') => void;
   openModal: () => void;
   closeModal: () => void;
 };
@@ -25,10 +29,18 @@ const initialData: Modal = {
 
 export const useModalTodo = create<ModalTodo>((set) => ({
   modal: initialData,
+  type: 'add',
   isShow: false,
+
   setModal: (newData: Modal) => {
     set({ modal: newData });
   },
+
+  resetModal: () => set({ modal: initialData, type: 'add' }),
+
+  setModalType: (type: 'add' | 'edit') => set({ type }),
+
   openModal: () => set({ isShow: true }),
+
   closeModal: () => set({ isShow: false })
 }));
