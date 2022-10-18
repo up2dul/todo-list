ad1838565ea1ac505bdade0f48c022e686669294
abdul <malik301002@gmail.com>
2022-10-18 12:56:51 +0700

    refactor(types): 💡 add optional to type

--- src/types/index.ts
+++ src/types/index.ts
@@ -26,7 +26,7 @@ export type TodoData = {
   title: string | undefined;
   is_active?: boolean;
   priority: PriorityType;
-  activity_group_id: number;
+  activity_group_id?: number;
 };
 
 export type PriorityType = 'very-high' | 'high' | 'medium' | 'low' | 'very-low';
