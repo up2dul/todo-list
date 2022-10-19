f7f8957f4845b5498f7510d7815df3f01068b976
abdul <malik301002@gmail.com>
2022-10-19 10:43:17 +0700

    feat(comps): 🎸 add event type on click to TrashButton

--- src/components/atoms/TrashButton.tsx
+++ src/components/atoms/TrashButton.tsx
@@ -1,15 +1,17 @@
+/* eslint-disable no-unused-vars */
+import { MouseEvent } from 'react';
 import { TbTrash } from 'react-icons/tb';
 
 type TrashButtonProps = {
   buttonType: 'activity' | 'todo';
-  onClick: () => void;
+  onClick: (e: MouseEvent<HTMLElement>) => void;
 };
 
 export const TrashButton = ({ buttonType, onClick }: TrashButtonProps) => (
   <span
     data-cy={`${buttonType}-item-delete-button`}
     title={`Delete ${buttonType}`}
-    onClick={() => onClick()}>
+    onClick={onClick}>
     <TbTrash className='cursor-pointer text-2xl text-dark-3 hover:text-dark-1' />
   </span>
 );
