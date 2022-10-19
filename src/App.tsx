fa23b69881e62f35ab5d3352388e7ae3bdab6034
abdul <malik301002@gmail.com>
2022-10-20 00:38:37 +0700

    feat(.): 🎸 add React.lazy import and React.Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
+import { Suspense, lazy } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { Detail, Dashboard } from '@/pages';
+import { Spinner } from '@/components/layouts';
+
+const Dashboard = lazy(() => import('@/pages/Dashboard'));
+const Detail = lazy(() => import('@/pages/Detail'));
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Dashboard />} />
-        <Route path='/detail/:activityId' element={<Detail />} />
-      </Routes>
-    </BrowserRouter>
+    <Suspense fallback={<Spinner />}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/detail/:activityId' element={<Detail />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 };
 
