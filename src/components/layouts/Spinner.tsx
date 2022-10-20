396782b146c1facc4e2beb7bdef7cd28278eb85f
abdul <malik301002@gmail.com>
2022-10-20 15:17:57 +0700

    feat(comps): 🎸 wrap Spinner with BaseLayout

--- src/components/layouts/Spinner.tsx
+++ src/components/layouts/Spinner.tsx
@@ -1,13 +1,16 @@
 import { SpinnerCircular } from 'spinners-react';
 
+import { BaseLayout } from '@/components/layouts';
+
 export const Spinner = () => (
-  <div className='flex h-screen w-full items-center justify-center'>
+  <BaseLayout>
     <SpinnerCircular
       size={50}
       thickness={170}
       speed={150}
       color='#16ABF8'
       secondaryColor='#ffffff'
+      className='mx-auto'
     />
-  </div>
+  </BaseLayout>
 );
