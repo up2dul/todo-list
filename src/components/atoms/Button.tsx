052c311183026e4ea45136e1663cfb05ed92481a
abdul <malik301002@gmail.com>
2022-10-24 10:20:39 +0700

    feat(comps): 🎸 add active css classes state on Button

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -11,9 +11,9 @@ type ButtonProps = {
 export const Button = ({ children, cy, color, onClick }: ButtonProps) => {
   const classes: string = clsx(
     'flex h-[54px] w-[150px] items-center justify-center rounded-full text-lg font-medium',
-    color === 'primary' && 'bg-primary text-light-1 hover:bg-primary/80',
-    color === 'danger' && 'bg-danger text-light-1 hover:bg-danger/80',
-    color === 'light' && 'bg-light-2 text-dark-1 hover:bg-dark-1/20',
+    color === 'primary' && 'bg-primary text-light-1 hover:bg-primary/70 active:bg-primary/90',
+    color === 'danger' && 'bg-danger text-light-1 hover:bg-danger/70 active:bg-danger/90',
+    color === 'light' && 'bg-light-2 text-dark-1 hover:bg-dark-1/10 active:bg-dark-1/20',
     color === 'disabled' && 'bg-secondary text-dark-3 cursor-not-allowed'
   );
 
